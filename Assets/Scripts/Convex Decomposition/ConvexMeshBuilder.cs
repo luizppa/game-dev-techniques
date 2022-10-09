fc2f888801469483426cb0678f7721da5d2611ee
luizppa <luizphilippep@gmail.com>
2022-10-09 09:52:23 -0300

    :construction: Calculating upper confidence bound

--- Assets/Scripts/Convex Decomposition/ConvexMeshBuilder.cs
+++ Assets/Scripts/Convex Decomposition/ConvexMeshBuilder.cs
@@ -19,6 +19,117 @@ public enum ConcavityMetric
   BILATERAL_HAUSDORFF,
 }
 
+public static class MeshHelper
+{
+  static readonly float K = .3f; // factor for the cocavity metric
+
+  public static Mesh ConvexHull(Mesh mesh)
+  {
+    List<Vector3> vertices = new List<Vector3>(mesh.vertices);
+    List<Vector3> hullVertices = new List<Vector3>();
+    List<int> hullTriangles = new List<int>();
+    List<Vector3> hullNormals = new List<Vector3>();
+
+    GK.ConvexHullCalculator convexHullCalculator = new GK.ConvexHullCalculator();
+    convexHullCalculator.GenerateHull(vertices, false, ref hullVertices, ref hullTriangles, ref hullNormals);
+    Mesh convexHull = new Mesh();
+    convexHull.vertices = hullVertices.ToArray();
+    convexHull.triangles = hullTriangles.ToArray();
+    convexHull.normals = hullNormals.ToArray();
+    convexHull.RecalculateNormals();
+    return convexHull;
+  }
+
+  public static float Volume(Mesh mesh)
+  {
+    float volume = 0;
+    Vector3[] vertices = mesh.vertices;
+    int[] triangles = mesh.triangles;
+    for (int i = 0; i < mesh.triangles.Length; i += 3)
+    {
+      Vector3 p1 = vertices[triangles[i + 0]];
+      Vector3 p2 = vertices[triangles[i + 1]];
+      Vector3 p3 = vertices[triangles[i + 2]];
+      volume += SignedVolumeOfTriangle(p1, p2, p3);
+    }
+    return Mathf.Abs(volume);
+  }
+
+  // ============================= Math shenanigans ============================= //
+
+  public static float SignedVolumeOfTriangle(Vector3 p1, Vector3 p2, Vector3 p3)
+  {
+    float v321 = p3.x * p2.y * p1.z;
+    float v231 = p2.x * p3.y * p1.z;
+    float v312 = p3.x * p1.y * p2.z;
+    float v132 = p1.x * p3.y * p2.z;
+    float v213 = p2.x * p1.y * p3.z;
+    float v123 = p1.x * p2.y * p3.z;
+    return (1.0f / 6.0f) * (-v321 + v231 + v312 - v132 - v213 + v123);
+  }
+
+  public static float CalculateConcavity(Mesh mesh, ConcavityMetric metric)
+  {
+    Mesh convexHull = ConvexHull(mesh);
+    switch (metric)
+    {
+      case ConcavityMetric.VOLUME:
+        return Volume(mesh) / Volume(convexHull);
+      // case ConcavityMetric.AREA:
+      //   return Area(mesh) / Area(convexHull);
+      // case ConcavityMetric.BOUNDING_BOX:
+      //   return BoundingBox(mesh).sqrMagnitude / BoundingBox(convexHull).sqrMagnitude;
+      case ConcavityMetric.HAUSDORFF:
+        return HausdorffDistance(mesh, convexHull);
+      case ConcavityMetric.BILATERAL_HAUSDORFF:
+        return Mathf.Max(HausdorffDistance(mesh, convexHull), K * Rv(mesh, convexHull));
+      default:
+        return HausdorffDistance(mesh, convexHull);
+    }
+  }
+
+  public static float Rv(Mesh mesh, Mesh convexHull)
+  {
+    float volume = Volume(mesh);
+    float hullVolume = Volume(convexHull);
+    return Mathf.Pow(((3f * hullVolume) - volume) / (4f * Mathf.PI), 1f / 3f);
+  }
+
+  public static float HausdorffDistance(Mesh meshA, Mesh meshB)
+  {
+    return Mathf.Max(OneSidedDistance(meshA.vertices, meshB.vertices), OneSidedDistance(meshB.vertices, meshA.vertices));
+  }
+
+  public static float OneSidedDistance(Vector3[] surfaceA, Vector3[] surfaceB)
+  {
+    float max = 0f;
+    foreach (Vector3 point in surfaceA)
+    {
+      float distance = DistanceFromSurface(point, surfaceB);
+      if (distance > max)
+      {
+        max = distance;
+      }
+    }
+    return max;
+  }
+
+  public static float DistanceFromSurface(Vector3 point, Vector3[] surfacePoints)
+  {
+    float min = Mathf.Infinity;
+    foreach (Vector3 sPoint in surfacePoints)
+    {
+      float distance = Vector3.Distance(point, sPoint);
+      if (distance < min)
+      {
+        min = distance;
+      }
+    }
+    return min;
+  }
+
+}
+
 public class MonteCarloTreeNode
 {
   public List<Mesh> components = new List<Mesh>();
@@ -31,7 +142,10 @@ public class MonteCarloTreeNode
   private int depth = 0;
   private int height = 1;
   private int triedPlanes = 0;
-  private float upperConfidenceBound = -1.0f;
+  private float upperConfidenceBound = -1f;
+  private float value = -1f;
+
+  private readonly float C = .5f; // exploration constant
 
   public MonteCarloTreeNode(int planesPerDimension, List<Mesh> components)
   {
@@ -92,10 +206,31 @@ public class MonteCarloTreeNode
   {
     if (upperConfidenceBound < 0f)
     {
-      // TODO: calculate upper confidence bound
+      upperConfidenceBound = this.Value();
+      if (parent != null)
+      {
+        upperConfidenceBound += C * Mathf.Sqrt((2f * Mathf.Log(parent.triedPlanes)) / triedPlanes);
+      }
     }
 
     return upperConfidenceBound;
+  }
+
+  public float Value()
+  {
+    if (value < 0f)
+    {
+      foreach (Mesh mesh in components)
+      {
+        float concavity = MeshHelper.CalculateConcavity(mesh, ConcavityMetric.HAUSDORFF);
+        float candidateValue = 1f / (1f + concavity);
+        if (candidateValue > value)
+        {
+          value = candidateValue;
+        }
+      }
+    }
+    return value;
   }
 
   private int[] GetPlaneValue(int planeIndex)
@@ -144,8 +279,7 @@ public class ConvexMeshBuilder : MonoBehaviour
   private float concavity = 0f;
 
   // Constants 
-  readonly float K = 1f; // factor for the cocavity metric
-  readonly int M = 5; // factor for the number of candidate planes
+  readonly int M = 20; // factor for the number of candidate planes
 
   void Start()
   {
@@ -165,15 +299,15 @@ public class ConvexMeshBuilder : MonoBehaviour
 
   void CalculateProperties()
   {
-    convexHull = ConvexHull(mesh);
-    volume = Volume(mesh) * meshFilter.transform.lossyScale.x;
-    hullVolume = Volume(convexHull) * meshFilter.transform.lossyScale.x;
-    concavity = CalculateConcavity(mesh, convexHull);
+    convexHull = MeshHelper.ConvexHull(mesh);
+    volume = MeshHelper.Volume(mesh) * meshFilter.transform.lossyScale.x;
+    hullVolume = MeshHelper.Volume(convexHull) * meshFilter.transform.lossyScale.x;
+    concavity = MeshHelper.CalculateConcavity(mesh, metric);
   }
 
   // ============================= Algorithms ============================= //
 
-  Plane MonteCarloTreeSearch(Mesh mesh, int iterations, int depth)
+  Plane MonteCarloTreeSearch(Mesh mesh, int iterations = 500, int depth = 4)
   {
     MonteCarloTreeNode root = new MonteCarloTreeNode(M, new List<Mesh> { mesh });
     MonteCarloTreeNode current = root;
@@ -203,7 +337,7 @@ public class ConvexMeshBuilder : MonoBehaviour
     MonteCarloTreeNode current = node;
     while (current.GetDepth() < depth)
     {
-      Mesh mesh = current.components.OrderBy(c => CalculateConcavity(c)).First();
+      Mesh mesh = current.components.OrderBy(c => MeshHelper.CalculateConcavity(c, metric)).First();
       if (current.GetChildCount() == (3 * M))
       {
         current = current.GetChildren().OrderBy(c => c.UpperConfidenceBound()).First();
@@ -245,8 +379,7 @@ public class ConvexMeshBuilder : MonoBehaviour
     while (queue.Count > 0)
     {
       Mesh m = queue.Dequeue();
-      Mesh hull = ConvexHull(m);
-      float concavity = CalculateConcavity(m, hull);
+      float concavity = MeshHelper.CalculateConcavity(m, metric);
       if (concavity < treshold)
       {
         meshes.Add(m);
@@ -260,114 +393,6 @@ public class ConvexMeshBuilder : MonoBehaviour
       }
     }
     return meshes;
-  }
-
-  Mesh ConvexHull(Mesh mesh)
-  {
-    List<Vector3> vertices = new List<Vector3>(mesh.vertices);
-    List<Vector3> hullVertices = new List<Vector3>();
-    List<int> hullTriangles = new List<int>();
-    List<Vector3> hullNormals = new List<Vector3>();
-
-    GK.ConvexHullCalculator convexHullCalculator = new GK.ConvexHullCalculator();
-    convexHullCalculator.GenerateHull(vertices, false, ref hullVertices, ref hullTriangles, ref hullNormals);
-    Mesh convexHull = new Mesh();
-    convexHull.vertices = hullVertices.ToArray();
-    convexHull.triangles = hullTriangles.ToArray();
-    convexHull.normals = hullNormals.ToArray();
-    convexHull.RecalculateNormals();
-    return convexHull;
-  }
-
-  float Volume(Mesh mesh)
-  {
-    float volume = 0;
-    Vector3[] vertices = mesh.vertices;
-    int[] triangles = mesh.triangles;
-    for (int i = 0; i < mesh.triangles.Length; i += 3)
-    {
-      Vector3 p1 = vertices[triangles[i + 0]];
-      Vector3 p2 = vertices[triangles[i + 1]];
-      Vector3 p3 = vertices[triangles[i + 2]];
-      volume += SignedVolumeOfTriangle(p1, p2, p3);
-    }
-    return Mathf.Abs(volume);
-  }
-
-  float SignedVolumeOfTriangle(Vector3 p1, Vector3 p2, Vector3 p3)
-  {
-    float v321 = p3.x * p2.y * p1.z;
-    float v231 = p2.x * p3.y * p1.z;
-    float v312 = p3.x * p1.y * p2.z;
-    float v132 = p1.x * p3.y * p2.z;
-    float v213 = p2.x * p1.y * p3.z;
-    float v123 = p1.x * p2.y * p3.z;
-    return (1.0f / 6.0f) * (-v321 + v231 + v312 - v132 - v213 + v123);
-  }
-
-  // ============================= Math shenanigans ============================= //
-
-  float CalculateConcavity(Mesh mesh)
-  {
-    return CalculateConcavity(mesh, ConvexHull(mesh));
-  }
-
-  float CalculateConcavity(Mesh mesh, Mesh convexHull)
-  {
-    switch (metric)
-    {
-      case ConcavityMetric.VOLUME:
-        return Volume(mesh) / Volume(convexHull);
-      // case ConcavityMetric.AREA:
-      //   return Area(mesh) / Area(convexHull);
-      // case ConcavityMetric.BOUNDING_BOX:
-      //   return BoundingBox(mesh).sqrMagnitude / BoundingBox(convexHull).sqrMagnitude;
-      case ConcavityMetric.HAUSDORFF:
-        return HausdorffDistance(mesh, convexHull);
-      case ConcavityMetric.BILATERAL_HAUSDORFF:
-        return Mathf.Max(HausdorffDistance(mesh, convexHull), K * Rv(mesh, convexHull));
-      default:
-        return HausdorffDistance(mesh, convexHull);
-    }
-
-  }
-
-  float Rv(Mesh mesh, Mesh convexHull)
-  {
-    return Mathf.Pow(((3f * hullVolume) - volume) / (4f * Mathf.PI), 1f / 3f);
-  }
-
-  float HausdorffDistance(Mesh meshA, Mesh meshB)
-  {
-    return Mathf.Max(OneSidedDistance(meshA.vertices, meshB.vertices), OneSidedDistance(meshB.vertices, meshA.vertices));
-  }
-
-  float OneSidedDistance(Vector3[] surfaceA, Vector3[] surfaceB)
-  {
-    float max = 0f;
-    foreach (Vector3 point in surfaceA)
-    {
-      float distance = DistanceFromSurface(point, surfaceB);
-      if (distance > max)
-      {
-        max = distance;
-      }
-    }
-    return max;
-  }
-
-  float DistanceFromSurface(Vector3 point, Vector3[] surfacePoints)
-  {
-    float min = Mathf.Infinity;
-    foreach (Vector3 sPoint in surfacePoints)
-    {
-      float distance = Vector3.Distance(point, sPoint);
-      if (distance < min)
-      {
-        min = distance;
-      }
-    }
-    return min;
   }
 
   // ============================= Gizmos ============================= //
